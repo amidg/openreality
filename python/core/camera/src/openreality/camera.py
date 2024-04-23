39dcc60f9cd9032174d075aef48852ce2a580416
Dmitrii Gusev <amidg4x4dima@gmail.com>
2024-04-23 12:55:49 -0700

    camera module can now use any number of cameras

--- python/core/camera/src/openreality/camera.py
+++ python/core/camera/src/openreality/camera.py
@@ -8,83 +8,143 @@ from time import time
 import multiprocessing
 from multiprocessing import shared_memory
 
-from typing import Literal, get_args
+from typing import Literal, List, get_args
+
+ROTATION_TYPES = Literal[
+    cv2.ROTATE_90_CLOCKWISE,
+    cv2.ROTATE_180,
+    cv2.ROTATE_90_COUNTERCLOCKWISE
+]
 
 """
     Sample camera class.
-    This class runs a multiprocessing.Process and streams image from the camera device, usually /dev/videoX to the shared memory.
-    This shared memory object can be accessed by other python programs
+    It allows to start camera with specified resolution and FPS
 """
-class Camera(multiprocessing.Process):
-    _ROTATION_TYPES = Literal[cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE]
+class Camera():
     def __init__(
         self,
         device: int, # 1 for /dev/video1
         width: int = 1920,
         height: int = 1080,
         fps: float = 30,
-        rotation: _ROTATION_TYPES = None # cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_180, cv2.ROTATE_90_COUNTERCLOCKWISE
     ):
-        super().__init__()
+        # camera parameters
         self._device = device
-        self._memory = f"cam{device}"
         self._width = width
         self._height = height
         self._fps = fps
+
+        # OpenCV capture parameters
+        self._cap = cv2.VideoCapture(self._device)
+        self._cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self._cap.set(cv2.CAP_PROP_FRAME_WIDTH, self._width)
+        self._cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self._height)
+        self._cap.set(cv2.CAP_PROP_FPS, self._fps)
+
+    @property
+    def cap(self):
+        return self._cap
+
+    @property
+    def width(self):
+        return self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    @property
+    def height(self):
+        return self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+"""
+   Capture class that combines camera stream from N cameras into one buffer 
+"""
+class Capture(multiprocessing.Process):
+    def __init__(
+        self,
+        cameras: List[Camera],
+        rotation: ROTATION_TYPES = None
+    ):
+        super().__init__()
+        self._cam_list = cameras
         self._rotation = None
+        self._memory = "camera"
         if rotation is not None:
             try:
-                assert rotation in get_args(self._ROTATION_TYPES)
+                assert rotation in get_args(ROTATION_TYPES)
                 self._rotation = rotation
             except AssertionError:
                 # TODO: add logger handler
                 print(f"Incorrect rotation requested {rotation}: must be cv2.ROTATE_XX_YY type")
 
     def run(self):
-        # create capture device
-        cap = cv2.VideoCapture(self._device, cv2.CAP_V4L2)
-        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self._width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self._height)
-        cap.set(cv2.CAP_PROP_FPS, self._fps)
+        # check if all cameras are ready
+        cam_ready = True
+        for cam in self._cam_list:
+            cam_ready = cam_ready and cam.cap.isOpened()
+    
+        # if not ready, we quit
+        # TODO: RaiseError
+        if not cam_ready:
+            exit()
 
-        # create frame for shared memory access
-        if cap.isOpened():
-            ret, frame = cap.read() # get test frame to create shared memory object
-            if not ret:
-                exit()
+        # if cameras are ready, we need to calculate the size of the buffer
+        # TODO: add handler for the not same size of the frame
+        all_cam_frames: List[np.ndarray] = []
+        for cam in self._cam_list:
+            if cam.cap.grab():
+                #TODO: Handler of the errors here
+                ret, frame = cam.cap.retrieve(0)
+
             # Flip frame if necessary
             if self._rotation is not None:
                 frame = cv2.rotate(frame, self._rotation)
-            shm = shared_memory.SharedMemory(
-                create=True, size=frame.nbytes, name=self._memory
-            )
-            frame_shared = np.ndarray(frame.shape, dtype=frame.dtype, buffer=shm.buf)
-        else:
-            exit()
+
+            # start creating stack of images
+            all_cam_frames.append(frame)
+        test_buf_frame = np.vstack(tuple(frame for frame in all_cam_frames))
+        shm = shared_memory.SharedMemory(
+            create=True, size=test_buf_frame.nbytes, name=self._memory
+        )
+        frame_buffer = np.ndarray(
+            test_buf_frame.shape,
+            dtype=test_buf_frame.dtype,
+            buffer=shm.buf
+        )
 
         # stream video
         # TODO: add handler when closed
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            # Flip frame if necessary
-            if self._rotation is not None:
-                frame = cv2.rotate(frame, self._rotation)
-            np.copyto(frame_shared, frame)  # Copy frame to shared memory
+        cam_frames: List[np.ndarray] = []
+        while cam_ready:
+            # get all frames
+            for camera in self._cam_list:
+                if camera.cap.grab():
+                    ret, frame = cam.cap.retrieve(0)
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    # Flip frame if necessary
+                    if self._rotation is not None:
+                        frame = cv2.rotate(frame, self._rotation)
+                    cam_frames.append(frame)
+                else:
+                    cam_ready = False
+                    break
+    
+            # build buffer
+            buffer = np.vstack(tuple(frame for frame in cam_frames))
+            np.copyto(frame_buffer, buffer)  # Copy frame to shared memory
+            cam_frames = []
 
         # stop opencv stream
-        cap.release()
+        for cam in self._cam_list:
+            cam.cap.release()
         shm.close()
         shm.unlink()
 
         
 # demo code to run this separately
 if __name__ == "__main__":
-    # start device in desired mode
-    test_cam_left = Camera(device=0, rotation=cv2.ROTATE_90_CLOCKWISE)
-    test_cam_left.start()
-    test_cam_right = Camera(device=1, rotation=cv2.ROTATE_90_CLOCKWISE)
-    test_cam_right.start()
+    # start create list of cameras
+    cam_left = Camera(device=0)
+    cam_right = Camera(device=2)
+    cameras = [cam_left, cam_right]
+
+    # create capture session
+    capture_session = Capture(cameras=cameras, rotation=cv2.ROTATE_90_CLOCKWISE)
+    capture_session.start()
