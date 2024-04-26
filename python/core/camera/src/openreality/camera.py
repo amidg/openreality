23f0439d5d6678cb9098d2d250990c7d619863df
Dmitrii Gusev <amidg4x4dima@gmail.com>
2024-04-25 19:45:23 -0700

    dual camera thread

--- python/core/camera/src/openreality/camera.py
+++ python/core/camera/src/openreality/camera.py
@@ -6,6 +6,76 @@ from typing import Literal, List, Tuple, get_args
 # multiprocessing
 import multiprocessing
 from multiprocessing import shared_memory
+import threading
+
+class DualCamera():
+    def __init__(
+        self,
+    ):
+        #super().__init__()
+
+        # camera parameters
+        self._resolution = (1280, 720)
+        self._fps = 30
+
+        # camera1
+        self._cap1 = cv2.VideoCapture(f"/dev/video0", cv2.CAP_V4L2)
+        self._cap1.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self._cap1.set(cv2.CAP_PROP_FRAME_WIDTH, self._resolution[0])
+        self._cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, self._resolution[1])
+        self._cap1.set(cv2.CAP_PROP_FPS, self._fps)
+
+        # camera2
+        self._cap2 = cv2.VideoCapture(f"/dev/video2", cv2.CAP_V4L2)
+        self._cap2.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self._cap2.set(cv2.CAP_PROP_FRAME_WIDTH, self._resolution[0])
+        self._cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, self._resolution[1])
+        self._cap2.set(cv2.CAP_PROP_FPS, self._fps)
+
+        # data
+        self._frame_left: np.ndarray = None
+        self._left_ready = False
+        self._frame_right: np.ndarray = None
+        self._right_ready = False
+
+        # stream
+        left_stream = threading.Thread(target=self._left_cam_read)
+        left_stream.start()
+
+        right_stream = threading.Thread(target=self._right_cam_read)
+        right_stream.start()
+
+        # performance
+        self._ctime_left = 0
+        self._ptime_left = 0
+        self._fps_left = 0
+
+        self._ctime_right = 0
+        self._ptime_right = 0
+        self._fps_right = 0
+
+    def _left_cam_read(self):
+        while True:
+            self._left_ready, self._frame_left = self._cap1.read()
+
+            # calculate fps
+            self._ctime_left = time.time()
+            self._fps_left = 1/(self._ctime_left-self._ptime_left)
+            self._ptime_left = self._ctime_left
+
+    def _right_cam_read(self):
+        while True:
+            self._right_ready, self._right_frame = self._cap2.read()
+
+            # calculate fps
+            self._ctime_right = time.time()
+            self._fps_right = 1/(self._ctime_right-self._ptime_right)
+            self._ptime_right = self._ctime_right
+
+    def run(self):
+        while True:
+            print(f"FPS: LEFT {self._fps_left} / RIGHT {self._fps_right}")
+
 
 """
     Sample camera class.
@@ -128,7 +198,11 @@ class Camera():
 # demo code to run this separately
 if __name__ == "__main__":
     # test cam
-    cam_left = Camera(device=0, resolution=(1280, 720), name="left_eye")
-    cam_left.run()
+    #cam_left = Camera(device=0, resolution=(1280, 720), name="left_eye")
+    #cam_left.run()
     #cam_right = Camera(device=2, resolution=(1280, 720), name="right_eye")
     #cam_right.start()
+
+    # dual camera test
+    dual_camera = DualCamera()
+    dual_camera.run()
